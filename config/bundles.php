68bac89f75aa59fab54af32f4d9cd9234d982d10
Javier Espinosa <javespalf@gmail.com>
2019-12-20 11:15:50 +0100

    require league tactician

--- config/bundles.php
+++ config/bundles.php
@@ -1,5 +1,8 @@
 <?php
 
+declare(strict_types=1);
+
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    League\Tactician\Bundle\TacticianBundle::class => ['all' => true],
 ];
