e178174ce31cfe407b023af4044e1c3dc882ca54
Javier Espinosa <javespalf@gmail.com>
2019-12-21 12:46:38 +0100

    composer require doctrine/dbal + bundle

--- config/bundles.php
+++ config/bundles.php
@@ -1,8 +1,7 @@
 <?php
 
-declare(strict_types=1);
-
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     League\Tactician\Bundle\TacticianBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
 ];
